575d5548b8fa5652732859d1fd68d8ad9a6ef7ed
AndreusSH <andreamammoliti@yahoo.it>
2024-04-07 12:34:14 +0200

    Handler middleware functions

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 //const http = require('http')
-const express = require('express');
-const path = require('path');
-
+const express = require('express')
+const path = require('path')
 
 const app = express()
 const port = process.env.PORT || 8080
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'templates', '/home.html'))
 })
+app.get('/about', function (req, res) {
+  res.sendFile(path.join(__dirname, 'templates', '/about.html'))
+})
+app.get('/contact', function (req, res) {
+  res.sendFile(path.join(__dirname, 'templates', '/contact.html'))
+})
+app.use(function(req, res) {
+    res.status(404).sendFile(path.join(__dirname, 'templates', '/error.html'));
+  });
 
 app.listen(port)
 console.log('Server started at http://localhost:' + port)
