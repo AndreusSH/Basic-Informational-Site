ada45edd7870a3d2a8b0fa930d11a6b9506b07ed
AndreusSH <andreamammoliti@yahoo.it>
2024-04-07 11:57:01 +0200

    define different url for each page

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-const http = require("http"); 
-//create a server object: 
-http 
-  .createServer(function (req, res) { 
-    res.write("<h1>Hello World!!!</h1>");  
-    //write a response to the client 
-     
-    res.end();  
-    //end the response 
-  }) 
-  .listen(8080);  
-//Server runs on localhost:8080 
+const http = require('http')
+//create a server object:
+http
+  .createServer(function (req, res) {
+    if (req.url === '/') {
+      res.write('<h1>Hello to our home page</h1>')
+    } else if (req.url === '/about') {
+      res.write('<h1>Short history about me</h1>')
+    } else if (req.url === '/contact') {
+      res.write('<h1>Contact me me</h1>')
+    } else {
+      res.write('Error')
+    }
+    res.end() //end the response
+  })
+  .listen(8080)
+//Server runs on localhost:8080
